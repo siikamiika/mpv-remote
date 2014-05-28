a531d4f2ef2f4b1608d28164e0e8dd180cc2e428
siikamiika <siikamiika@users.noreply.github.com>
2014-05-28 17:47:14 +0300

    enhancements and code cleanup

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ from urllib.parse import urlparse, parse_qsl, quote
 from pathlib import Path
 from functools import cmp_to_key
 from subprocess import Popen
+from os import sep
+from os.path import expanduser
 
 base = """<head>
 <meta name="viewport" content="width=device-width, user-scalable=no" />
@@ -35,38 +37,41 @@ class MpvRequestHandler(BaseHTTPRequestHandler):
         d = Path(path)
         if not d.is_dir():
             self.respond_notfound()
-        else:
-            listing = ['<h1>{}</h1><hr><ul>'.format(d)]
+            return
+        parts = str(d).split(sep)
+        links = '<a href="/?dir=/">(root)</a>' + sep.join(
+            '<a href="/?dir={0}/">{1}</a>'.format(
+                sep.join(parts[:parts.index(d_)+1]), d_)
+                for d_ in parts
+            )
+        listing = ['<h1>{}</h1><hr><ul>'.format(links)]
 
-            def sort_cmp(a, b):
-                if a.is_dir() and b.is_dir():
-                    return (a > b)*2 - 1
-                elif a.is_dir() and b.is_file():
-                    return -1
-                elif a.is_file() and b.is_dir():
-                    return 1
-                elif a.is_file and b.is_file():
-                    return (a > b)*2 - 1
-                else:
-                    return 0
-                    
-            for x in sorted(d.iterdir(), key=cmp_to_key(lambda x, y: sort_cmp(x,y))):
-                link = quote(str(x))
-                text = str(x).split('/')[-1]
-                if x.is_file():
-                    vid = False
-                    if text.split('.')[-1] in ['avi', 'mp4', 'mkv', 'ogg', 'flv', 'm4v', 'mov', 'mpg', 'mpeg', 'wmv']:
-                        vid = True
-                    listing.append(
-                        '<li><a class="{cls}" href="?play={link}">{text}</a></li>'.format(
-                            cls=('video' if vid else 'file'), link=link, text=text))
-                elif x.is_dir():
-                    listing.append(
-                        '<li><a class="folder" href="?dir={link}/">{text}/</a></li>'.format(
-                            link=link, text=text))
-            listing.append('</ul>')
+        def sort_cmp(a, b):
+            if a.is_dir() == b.is_dir():
+                return (a > b)*2 - 1
+            elif a.is_dir():
+                return -1
+            elif a.is_file():
+                return 1
 
-            self.respond_ok((base + ''.join(listing)).encode())
+        for x in sorted(d.iterdir(), key=cmp_to_key(lambda x, y: sort_cmp(x,y))):
+            link = quote(str(x))
+            text = str(x).split('/')[-1]
+            if x.is_file():
+                vid = False
+                vid_ext = ['avi', 'mp4', 'mkv', 'ogg', 'flv', 'm4v', 'mov', 'mpg', 'mpeg', 'wmv']
+                if text.split('.')[-1] in vid_ext:
+                    vid = True
+                listing.append(
+                    '<li><a class="{cls}" href="/?play={link}">{text}</a></li>'.format(
+                        cls=('video' if vid else 'file'), link=link, text=text))
+            elif x.is_dir():
+                listing.append(
+                    '<li><a class="folder" href="/?dir={link}/">{text}/</a></li>'.format(
+                        link=link, text=text))
+        listing.append('</ul>')
+
+        self.respond_ok((base + ''.join(listing)).encode())
 
     def play_file(self, fpath):
         self.respond_ok('file playing...'.encode())
@@ -84,8 +89,10 @@ class MpvRequestHandler(BaseHTTPRequestHandler):
         elif play_path:
             self.play_file(play_path)
         elif self.path == '/':
-            self.respond_ok('asdf'.encode())
-
+            homedir = expanduser('~')
+            self.send_response(302)
+            self.send_header('Location', '/?dir='+quote(homedir))
+            self.end_headers()
         else:
             self.respond_notfound()
 
