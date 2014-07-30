b367feaf4a99942b629db482e15b668366f2e5bc
siikamiika <siikamiika@users.noreply.github.com>
2014-07-30 23:14:26 +0300

    simplify button generation, move directory listing to a separate class

--- server.py
+++ server.py
@@ -5,97 +5,66 @@ from functools import cmp_to_key
 from subprocess import call
 import os
 import string
-import json
 from os.path import expanduser, splitext, getmtime
 from threading import Thread
 import socket
 if os.name == 'nt': from ctypes import windll
 
-def lua_fn_str(fn, args):
-    return '{}({})'.format(fn, ','.join(['"{}"'.format(a) for a in args]))
+class DirectoryViewer(object):
 
-class MpvRequestHandler(BaseHTTPRequestHandler):
-
-    with open('template.html', 'r') as f: base = string.Template(f.read())
-    with open('layout.json', 'r') as f: layout = json.loads(f.read())
-    with open('config', 'r') as f:
-        config = ['--{}'.format(o) for o in f.read().splitlines()
-                  if '=' in o and not o.startswith('#')]
-
-    command_list = []
-    for row in layout['layout']:
-        for button in row:
-            command_list.append(button[1])
-    commands = {lua_fn_str(f, a) for f, a in command_list}
-
-    htmlcontrols = '<div class="remote"><div style="text-align:center;">'
-    for control in layout['layout']:
-        for button in control:
-            symbol, command = button
-            btn = '<a class="btn" href="/?control={cmd}"><i class="fa fa-3x fa-{sym}"></i></a> '
-            htmlcontrols += btn.format(
-                cmd=quote(lua_fn_str(command[0], command[1])),
-                sym=symbol
+    def __init__(self, path):
+        if path == 'WINROOT' and os.name == 'nt':
+            self.as_html = self.list_windows_drives()
+        else:
+            self.path = Path(path)
+            self.as_html = '<h1>{nav_links}</h1><hr><ul>{cont_links}</ul>'.format(
+                    nav_links=self.generate_navigation_links(),
+                    cont_links=self.generate_content_links(),
                 )
-        htmlcontrols += '</div><div style="text-align:center;">'
-    htmlcontrols += '</div></div>'
 
-    controls = { 'content': htmlcontrols }
-    controls = base.substitute(controls)
+    def list_windows_drives(self):
+        drives = []
+        bitmask = windll.kernel32.GetLogicalDrives()
+        for letter in map(chr, range(65, 91)):
+            if bitmask & 1:
+                drives.append(letter)
+            bitmask >>= 1
+        drive_link = '<li><a class="folder" href="/?dir={drive}%3A%5C"><i class="fa fa-folder"></i> {drive}:</a></li>'
+        return '<ul>' + ''.join([drive_link.format(drive=d) for d in drives]) + '</ul>'
 
-    def redirect(self, location):
-        self.send_response(302)
-        self.send_header('Location', location)
-        self.end_headers()
-
-    def respond_ok(self, data, content_type='text/html; charset=utf-8', age=0):
-        self.send_response(200)
-        self.send_header('Cache-Control', 'public, max-age={}'.format(age))
-        self.send_header('Content-Type', content_type)
-        self.end_headers()
-        self.wfile.write(data)
-
-    def respond_notfound(self, data=None):
-        self.send_response(404)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(data or '404'.encode())
-
-    def list_dir(self, path):
-        d = Path(path)
-        if not d.is_dir():
-            self.respond_notfound()
-            return
-        nav = '<a class="navlink" href="/?dir=WINROOT">(root)</a>|' if os.name == 'nt' else ''
-        nav += os.sep.join(
+    def generate_navigation_links(self):
+        navlinks = '<a class="navlink" href="/?dir=WINROOT">(root)</a>|' if os.name == 'nt' else ''
+        navlinks += os.sep.join(
             '<a class="navlink" href="/?dir={0}">{1}</a>'.format(
                 quote(str(d)), d.parts[-1])
-                for d in list(reversed(d.parents)) + [d]
+                for d in list(reversed(self.path.parents)) + [self.path]
             )
-        listing = ['<h1>{}</h1><hr><ul>'.format(nav)]
+        return navlinks
 
-        def sort(a, b):
-            def sort_file(c, d): return (str(c).lower() > str(d).lower()) - .5
-            try:
-                a.is_dir()
-                b.is_dir()
-            except Exception as e:
-                print(e)
-                return sort_file(a, b)
-            if a.is_dir():
-                if b.is_dir():
-                    return (getmtime(str(a)) < getmtime(str(b))) - .5
-                else:
-                    return -1
-            elif a.is_file():
-                if b.is_file():
-                    return sort_file(a, b)
-                else:
-                    return 1
+    def sort_compare(self, a, b):
+        def two_files(c, d): return (str(c).lower() > str(d).lower()) - .5
+        try:
+            a.is_dir()
+            b.is_dir()
+        except Exception as e:
+            print(e)
+            return two_files(a, b)
+        if a.is_dir():
+            if b.is_dir():
+                return (getmtime(str(a)) < getmtime(str(b))) - .5
             else:
-                return sort_file(a, b)
+                return -1
+        elif a.is_file():
+            if b.is_file():
+                return two_files(a, b)
+            else:
+                return 1
+        else:
+            return two_files(a, b)
 
-        for x in sorted(d.iterdir(), key=cmp_to_key(sort)):
+    def generate_content_links(self):
+        content = []
+        for x in sorted(self.path.iterdir(), key=cmp_to_key(self.sort_compare)):
             text = str(x).split(os.sep)[-1]
             if text.startswith('.'): continue
             link = quote(str(x))
@@ -123,13 +92,50 @@ class MpvRequestHandler(BaseHTTPRequestHandler):
                 fa = 'fa fa-folder'
             else:
                 continue
-            listing.append(
+            content.append(
                     '<li><a class="{cls}" href="/?{function}={link}"><i class="{fa}"></i> {text}</a></li>'.format(
                         function=function, cls=cls, fa=fa, link=link, text=text
                     )
                 )
-        listing.append('</ul>')
-        listing = ''.join(listing)
+        return ''.join(content)
+
+
+class MpvRequestHandler(BaseHTTPRequestHandler):
+
+    with open('template.html', 'r') as f: base = string.Template(f.read())
+    with open('buttons.html', 'r') as f: buttons = f.read()
+    with open('allowed', 'r') as f:
+        commands = [c for c in f.read().splitlines() if c]
+    with open('config', 'r') as f:
+        config = ['--{}'.format(o) for o in f.read().splitlines()
+                  if '=' in o and not o.startswith('#')]
+
+    controls = { 'content': buttons }
+    controls = base.substitute(controls)
+
+    def redirect(self, location):
+        self.send_response(302)
+        self.send_header('Location', location)
+        self.end_headers()
+
+    def respond_ok(self, data, content_type='text/html; charset=utf-8', age=0):
+        self.send_response(200)
+        self.send_header('Cache-Control', 'public, max-age={}'.format(age))
+        self.send_header('Content-Type', content_type)
+        self.end_headers()
+        self.wfile.write(data)
+
+    def respond_notfound(self, data=None):
+        self.send_response(404)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(data or '404'.encode())
+
+    def list_dir(self, path):
+        try:
+            listing = DirectoryViewer(path).as_html
+        except Exception as e:
+            return self.respond_notfound(str(e).encode())
         listing = self.base.substitute({'content': '<div class="listing">{}</div>'.format(listing)})
         self.respond_ok(listing.encode())
 
@@ -144,6 +150,30 @@ class MpvRequestHandler(BaseHTTPRequestHandler):
         Thread(target=call_mpv, args=(fpath,)).start()
         self.redirect('/control')
 
+    def serve_static(self):
+        requested = unquote(self.path[len('/static/'):])
+        if requested not in os.listdir('static'):
+            self.respond_notfound('file not found'.encode())
+        else:
+            try:
+                with open('static/'+requested, 'rb') as f:
+                    ct = {'.css': 'text/css'}
+                    self.respond_ok(
+                        f.read(),
+                        (ct.get(splitext(requested)[1]) or 'application/octet-stream'),
+                        315360000
+                        )
+            except Exception as e:
+                print(e)
+                self.respond_notfound('error reading file'.encode())
+
+    def control_mpv(self, command):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.sendto(command.encode(), ('localhost', 9876))
+        h = 1
+        if command == 'mp.command("stop")': h = 2
+        self.respond_ok('<script>history.go(-{});</script>'.format(h).encode())
+
     def do_GET(self):
 
         qs_list = dict(parse_qsl(urlparse(self.path).query))
@@ -152,43 +182,13 @@ class MpvRequestHandler(BaseHTTPRequestHandler):
         control_command = qs_list.get('control')
 
         if self.path.startswith('/static/'):
-            requested = unquote(self.path[len('/static/'):])
-            if requested not in os.listdir('static'):
-                self.respond_notfound('file not found'.encode())
-            else:
-                try:
-                    with open('static/'+requested, 'rb') as f:
-                        ct = {'.css': 'text/css'}
-                        self.respond_ok(
-                            f.read(),
-                            (ct.get(splitext(requested)[1]) or 'application/octet-stream'),
-                            315360000
-                            )
-                except Exception as e:
-                    print(e)
-                    self.respond_notfound('error reading file'.encode())
+            self.serve_static()
         elif dir_path:
-            if dir_path == 'WINROOT' and os.name == 'nt':
-                drives = []
-                bitmask = windll.kernel32.GetLogicalDrives()
-                for letter in map(chr, range(65, 91)):
-                    if bitmask & 1:
-                        drives.append(letter)
-                    bitmask >>= 1
-                drive_link = '<li><a class="folder" href="/?dir={drive}%3A%5C"><i class="fa fa-folder"></i> {drive}:</a></li>'
-                drive_links = '<ul>' + ''.join([drive_link.format(drive=d) for d in drives]) + '</ul>'
-                listing = self.base.substitute({'content': '<div class="listing">{}</div>'.format(drive_links)})
-                self.respond_ok(listing.encode())
-            else:
-                self.list_dir(dir_path)
+            self.list_dir(dir_path)
         elif play_path:
             self.play_file(play_path)
         elif control_command in self.commands:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.sendto(control_command.encode(), ('localhost', 9876))
-            h = 1
-            if control_command == 'mp.command("stop")': h = 2
-            self.respond_ok('<script>history.go(-{});</script>'.format(h).encode())
+            self.control_mpv(control_command)
         elif self.path == '/control':
             self.respond_ok(self.controls.encode())
         elif self.path == '/':
